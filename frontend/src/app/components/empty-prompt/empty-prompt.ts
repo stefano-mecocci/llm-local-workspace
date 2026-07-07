cf91edcf9bc7e9739e7db0da363f9a131a1730ef
Stefano Mecocci <mecocci15@gmail.com>
2026-07-07 16:16:11 +0200

    feat(fe): add prompt box in the welcome/empty screen

--- frontend/src/app/components/empty-prompt/empty-prompt.ts
+++ frontend/src/app/components/empty-prompt/empty-prompt.ts
@@ -1,8 +1,10 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { PromptBox } from "../prompt-box/prompt-box";
 
 @Component({
   selector: 'app-empty-prompt',
   templateUrl: './empty-prompt.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [PromptBox],
 })
-export class EmptyPromptComponent {}
+export class EmptyPromptComponent { }

db0d0cef6c64a1882b0bb9088d6819548f864f6b
Stefano Mecocci <mecocci15@gmail.com>
2026-07-07 17:11:08 +0200

    feat(fe): improve prompt box with placeholder, send button and model signal

--- frontend/src/app/components/prompt-box/prompt-box.ts
+++ frontend/src/app/components/prompt-box/prompt-box.ts
@@ -1,9 +1,26 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { Component, ChangeDetectionStrategy, signal, model } from '@angular/core';
+import { AutoResizeDirective } from "../../directives/auto-resize";
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-prompt-box',
   templateUrl: './prompt-box.html',
   styleUrl: './prompt-box.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [AutoResizeDirective, FormsModule],
 })
-export class PromptBox {}
+export class PromptBox {
+  readonly prompt = model('');
+
+  onEnterPressed(event: KeyboardEvent) {
+    if (event.key !== "Enter" || event.shiftKey) return;
+
+    event.preventDefault();
+    this.sendPrompt();
+  }
+
+  sendPrompt() {
+    console.log("Prompt sent to LLM: " + this.prompt().trim());
+    this.prompt.set('');
+  }
+}

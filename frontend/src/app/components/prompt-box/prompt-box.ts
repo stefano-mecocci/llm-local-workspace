3385200824c493a516d668b1b93c5e0ef65cd28e
Stefano Mecocci <mecocci15@gmail.com>
2026-07-08 17:08:01 +0200

    feat(fe): use select and butto from primeng in prompt box

--- frontend/src/app/components/prompt-box/prompt-box.ts
+++ frontend/src/app/components/prompt-box/prompt-box.ts
@@ -1,16 +1,30 @@
 import { Component, ChangeDetectionStrategy, signal, model } from '@angular/core';
 import { AutoResizeDirective } from "../../directives/auto-resize";
 import { FormsModule } from '@angular/forms';
+import { Select, SelectChangeEvent } from 'primeng/select';
+import { ButtonDirective } from 'primeng/button';
+import { ArrowUpIcon } from 'primeng/icons';
+
+type LlmModel = "gemma4:e2b" | "gemma4:e4b-mlx"
 
 @Component({
   selector: 'app-prompt-box',
   templateUrl: './prompt-box.html',
   styleUrl: './prompt-box.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [AutoResizeDirective, FormsModule],
+  imports: [AutoResizeDirective, FormsModule, Select, ButtonDirective, ArrowUpIcon],
 })
 export class PromptBox {
   readonly prompt = model('');
+  readonly availableModels: { label: string, model: LlmModel }[] = [
+    { label: "Gemma 4 (2B)", model: "gemma4:e2b" },
+    { label: "Gemma 4 (4B MLX)", model: "gemma4:e4b-mlx" }
+  ];
+  readonly choosedModel = signal<LlmModel>("gemma4:e2b");
+
+  changeModel(event: SelectChangeEvent) {
+    console.log(event.value);
+  }
 
   onEnterPressed(event: KeyboardEvent) {
     if (event.key !== "Enter" || event.shiftKey) return;

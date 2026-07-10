328262b33398398aadd55f41484db116f4a39bbd
Stefano Mecocci <mecocci15@gmail.com>
2026-07-10 18:07:29 +0200

    feat(fe): add markdown on ai messages

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -4,6 +4,7 @@ import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { routes } from './app.routes';
 import { providePrimeNG } from 'primeng/config';
 import CustomPreset from './theme';
+import { provideMarkdown } from 'ngx-markdown';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -15,5 +16,6 @@ export const appConfig: ApplicationConfig = {
       },
       license: "eyJpZCI6IjQ0NjU1ZDkxLWZiNGItNDFlMC04MzIyLWQwMDZjNzRmMWFiYSIsInByb2R1Y3QiOiJwcmltZXVpIiwidGllciI6ImNvbW11bml0eSIsInR5cGUiOiJkZXYiLCJpYXQiOjE3ODM1MjIwODgsImV4cCI6MTgxNTA1ODA4OH0.c4M-7P4fUZrBlHd9Q71iIdDaKtU99wfH_Yrj5MbCvLFRHENIMkSvA674gylttvUBOR9_Dza7OAARaPJVIKbIAw"
     }),
+    provideMarkdown(),
   ]
 };
--- frontend/src/app/components/ai-message/ai-message.ts
+++ frontend/src/app/components/ai-message/ai-message.ts
@@ -1,10 +1,12 @@
 import { Component, ChangeDetectionStrategy, input } from '@angular/core';
+import { MarkdownComponent } from 'ngx-markdown';
 
 @Component({
   selector: 'app-ai-message',
   templateUrl: './ai-message.html',
   styleUrl: './ai-message.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [MarkdownComponent]
 })
 export class AiMessage {
   readonly content = input.required<string>();

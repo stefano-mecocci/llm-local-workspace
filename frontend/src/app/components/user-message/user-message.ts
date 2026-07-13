3562b2a62cadf9afc4a5fb6b0a9c02167e4c815a
Stefano Mecocci <mecocci15@gmail.com>
2026-07-13 12:33:54 +0200

    feat(fe): enhance user message component with image overlay functionality

--- frontend/src/app/components/user-message/user-message.ts
+++ frontend/src/app/components/user-message/user-message.ts
@@ -1,10 +1,13 @@
-import { Component, ChangeDetectionStrategy, input, computed } from '@angular/core';
+import { Component, ChangeDetectionStrategy, input, computed, signal } from '@angular/core';
 import { ChatMessage } from '../../services/chat-state';
+import { ButtonModule } from 'primeng/button';
+import { Times } from '@primeicons/angular/times';
 
 @Component({
   selector: 'app-user-message',
   templateUrl: './user-message.html',
   styleUrl: './user-message.css',
+  imports: [ButtonModule, Times],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class UserMessage {
@@ -19,4 +22,14 @@ export class UserMessage {
       return "data:*/*;base64," + src;
     }
   });
+
+  readonly isOverlayOpen = signal(false);
+
+  openOverlay() {
+    this.isOverlayOpen.set(true);
+  }
+
+  closeOverlay() {
+    this.isOverlayOpen.set(false);
+  }
 }

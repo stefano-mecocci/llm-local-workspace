864c169b912254792bfad8be49a762f756feec68
Stefano Mecocci <mecocci15@gmail.com>
2026-07-13 10:58:48 +0200

    chore(fe): add random id testing function

---
+++ frontend/src/app/utils/randomAlphanumericString.ts
@@ -0,0 +1,10 @@
+function randomAlphaNumericString() {
+    const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let result = '';
+    for (let i = 0; i < 20; i++) {
+        result += chars.charAt(Math.floor(Math.random() * chars.length));
+    }
+    return result;
+}
+
+export default randomAlphaNumericString;
